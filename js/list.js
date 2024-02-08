dc1c7ec0d38c2b435b70b0fcc14895bdf75104db
Mau100404 <156386238+Mau100404@users.noreply.github.com>
2024-02-08 01:23:35 +0000

    cambios 2.1

--- js/list.js
+++ js/list.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
         var total = itemList.childElementCount + 1;
         var clone = template.content.cloneNode(true);
         clone.querySelector("[data-id='number']").textContent = `${total}`;
+        clone.querySelector("[data-id='image']").src = "https://pm1.narvii.com/6480/3795a9ab03a916314f8e0bf3fcd1649648b2b6e1_hq.jpg";
         clone.querySelector("[data-id='name']").textContent = "Morty";
         clone.querySelector("[data-id='status']").textContent = "Staus:";
         clone.querySelector("[data-id='especie']").textContent = "Especie:";
